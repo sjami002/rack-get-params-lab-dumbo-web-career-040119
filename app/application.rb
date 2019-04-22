0ad67720dfc49fa834ef947627b3454e010197ad
Shoptorshi Jamil <sjami002@gmail.com>
2019-04-22 20:17:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,6 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
+      if @@cart
       @@cart.each do |cart|
         resp.write "#{cart}\n"
       end
