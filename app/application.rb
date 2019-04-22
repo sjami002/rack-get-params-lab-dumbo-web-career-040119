10ba5b84a721c26b69f9404d85d759c722c1dcfb
Shoptorshi Jamil <sjami002@gmail.com>
2019-04-22 20:21:39 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,8 @@ class Application
       end
     end
     elsif req.path.match(/add/)
-      add_item = req.params
+      add_item = req.params['item']
+      resp.write handle_search(add_item)
     else
       resp.write "Path Not Found"
     end
